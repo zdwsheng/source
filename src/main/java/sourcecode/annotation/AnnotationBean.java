d08f992db5b6cd5926666a3d7ff21d08f08b2df8
18551061423 <zdwsheng@foxmail.com>
2019-07-07 17:04:36 +0800

    薪èè新增lombok

--- src/main/java/sourcecode/annotation/AnnotationBean.java
+++ src/main/java/sourcecode/annotation/AnnotationBean.java
@@ -1,14 +1,19 @@
 package sourcecode.annotation;
 
+import com.alibaba.fastjson.JSONObject;
+import lombok.Data;
+import lombok.ToString;
+
 /**
  * @author: 张灯皖
  * @name
- * @desc TODO
+ * @desc
  * @jdk
  * @group
  * @os
  * @date 2018/12/21
  */
+@Data
 public class AnnotationBean {
     @MyAnnotation(id = 1)
     private Integer id = 9;
@@ -17,32 +22,13 @@ public class AnnotationBean {
     @MyAnnotation(sex = '3')
     private char sex = '男';
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public char getSex() {
-        return sex;
-    }
-
-    public void setSex(char sex) {
-        this.sex = sex;
-    }
-
     @Override
     public String toString() {
-        return "toString--id: " + id + " name:" + name + " sex:" + sex;
+          return JSONObject.toJSONString(this);
+    }
+
+    public static void main(String[] args) {
+        AnnotationBean annotationBean = new AnnotationBean();
+        System.out.println(annotationBean);
     }
 }
