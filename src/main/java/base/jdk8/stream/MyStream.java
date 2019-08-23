4cb6e4646426441af128125f676d91f9209b360b
zdw <zhangdengwan@juxinli.com>
2019-08-23 15:35:13 +0800

    薪新增collect操作

--- src/main/java/base/jdk8/stream/MyStream.java
+++ src/main/java/base/jdk8/stream/MyStream.java
@@ -1,11 +1,9 @@
 package base.jdk8.stream;
 
 import base.jdk8.bean.User;
+import com.alibaba.fastjson.JSONObject;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.DoublePredicate;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -24,22 +22,29 @@ import static java.lang.System.out;
 public class MyStream {
     private static final MyStream instance = new MyStream();
 
+    private List<User> userList = new LinkedList<>();
+
+    {
+        userList.add(new User(5, "1", 'Y'));
+        userList.add(new User(20, "2", 'Y'));
+        userList.add(new User(4, "3", 'Y'));
+        userList.add(new User(4, "3", 'Y'));
+    }
+
     public static void main(String[] args) {
-       /* Map map = instance.listToMap();
-        System.out.println(JSONObject.toJSONString(map));*/
+        Map map = instance.listToMap();
+        System.out.println(JSONObject.toJSONString(map));
 //        instance.intStream();
-        instance.parallelStream();
+//        instance.parallelStream();
+//        instance.reduceStream();
+        instance.collect();
     }
 
     private Map listToMap() {
-        List<User> userList = new LinkedList<>();
-        userList.add(new User(5, "1", 'Y'));
-        userList.add(new User(2, "2", 'Y'));
-        userList.add(new User(4, "3", 'Y'));
-        userList.add(new User(4, "3", 'Y'));
         /*//key不能重复
         Map<Integer, User> userMap = userList.stream().collect(Collectors.toMap(user -> user.getAge(), Function.identity()));*/
-        Map<Integer, User> userMap = userList.stream().collect(Collectors.toMap(user -> user.getAge(), Function.identity(), (preUser, user) -> preUser));
+        Map<Integer, User> userMap = userList.stream().collect(Collectors.toMap(User::getAge, Function.identity(), (preUser, user) -> preUser));
+//        userMap = userList.stream().collect(HashMap::new, (m, v) -> m.put(v.getAge(), v), Map::putAll);
         return userMap;
     }
 
@@ -62,4 +67,39 @@ public class MyStream {
         numbers.parallelStream()
                 .forEachOrdered(out::println);
     }
+
+    private void reduceStream() {
+
+        Integer totalAge = userList.stream().map(User::getAge).filter(Objects::nonNull).reduce(0, (a, b) -> a + b);
+        out.println("totalAge:" + totalAge);
+        //未指定初始值，所以是返回的Optional
+        Optional<Integer> reduceAgeOptional = userList.stream().map(User::getAge).filter(Objects::nonNull).reduce((a, b) -> a + b);
+        out.println("totalAge:" + reduceAgeOptional.orElseGet(() -> 0));
+        Integer max = userList.stream().map(User::getAge).filter(Objects::nonNull).reduce(Integer::max).get();
+        out.println("最大值:" + max);
+    }
+
+    /**
+     * collect(Supplier<R> supplier,初始容器
+     * BiConsumer<R, ? super P_OUT> accumulator, 加入到容器操作
+     * BiConsumer<R, R> combiner) 多容器聚合操作
+     * collect(StringBuilder::new, StringBuilder::append,StringBuilder::append)
+     * <p>
+     * <p>
+     * <p>
+     * CollectorImpl(Supplier<A> supplier, 初始容器
+     * BiConsumer<A, T> accumulator, 加入到容器操作
+     * BinaryOperator<A> combiner, 多容器聚合操作
+     * Function<A,R> finisher,聚合后的结果操作
+     * Set<Characteristics> characteristics) 操作中便于优化的状态字段
+     */
+    private void collect() {
+        String collectAge = userList.stream().map(User::getAge).map(String::valueOf).collect(Collectors.joining(","));
+        out.println(collectAge);
+        String name = userList.stream().map(User::getName).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString();
+        out.println("name:" + name);
+        HashSet<User> userSet = userList.stream().collect(HashSet::new, Set::add, Set::addAll);
+        out.println(userSet.isEmpty());
+    }
+
 }
