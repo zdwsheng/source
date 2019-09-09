3afea3ef10ae5ea02ee47c378d4af8a6bf60e15e
zdw <zhangdengwan@juxinli.com>
2019-09-09 16:06:31 +0800

    注释

--- src/main/java/sourcecode/framework/spring/aop/CglibAop.java
+++ src/main/java/sourcecode/framework/spring/aop/CglibAop.java
@@ -10,13 +10,15 @@ import java.lang.reflect.Method;
  * @ClassName: CglibAop
  * @Author zdw
  * @Desc cglib基于继承实现代理，所以无法对final类和static,private方法进行代理
- *       可以代理protected方法，但是必须在同一个包下面(无意义，一般引用jar，不会再一个包下面)
+ * 可以代理protected方法，但是必须在同一个包下面(无意义，一般引用jar，不会再一个包下面) spring禁止代理protected
  * @Date 2019/1/20 9:10
  */
 public class CglibAop implements MethodInterceptor {
     private Object proxy;
 
-    public CglibAop(){}
+    public CglibAop() {
+    }
+
     public Object getProxyInstance(Object proxy) {
         this.proxy = proxy;
         Enhancer enhancer = new Enhancer();
